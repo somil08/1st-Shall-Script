dd2ddce2d3adeb2a7d1ad3652a47eacfb0a6c712
somil08 <Somilrathore0208@gmail.com>
2024-09-16 20:47:23 +0530

    first-update

---
+++ project1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+###################################
+#Author: Somil Rathore
+# Date: 16th-sep 
+# version: v1 
+# this script will report the AWS resource usage
+###################################
+    
+
+
+
